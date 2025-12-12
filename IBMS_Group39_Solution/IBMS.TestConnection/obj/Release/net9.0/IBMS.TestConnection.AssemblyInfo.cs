d5c53654008aeda098ffa8caabfc0059f11d2e6d
MuhammadKhan79 <26100138@lums.edu.pk>
2025-12-12 08:14:26 +0500

    .

--- IBMS_Group39_Solution/IBMS.TestConnection/obj/Release/net9.0/IBMS.TestConnection.AssemblyInfo.cs
+++ IBMS_Group39_Solution/IBMS.TestConnection/obj/Release/net9.0/IBMS.TestConnection.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("IBMS.TestConnection")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5ee680b676eca112f39609a96a9593e32468c6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("IBMS.TestConnection")]
